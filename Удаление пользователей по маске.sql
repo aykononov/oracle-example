db02dd9fa0f0c45c655144659e34503d2c2ec841
Александр Кононов <aykononov@yandex.ru>
2022-03-04 21:04:50 +0300

    Update Удаление пользователей по маске.sql

--- Удаление пользователей по маске.sql
+++ Удаление пользователей по маске.sql
@@ -11,3 +11,32 @@ BEGIN
    dbms_output.put_line(sqlerrm);
 END;
 /
+
+-- Смотрим пользователей(схемы)
+SELECT t.user_id, t.username, t.account_status, t.created
+FROM DBA_USERS t
+WHERE t.username LIKE 'INTEGRATION_TEST_%'
+  AND TRUNC(t.created) <= TRUNC(SYSDATE)
+ORDER BY t.created DESC;
+
+-- Удаление пользователей по маске
+BEGIN
+    FOR i IN (SELECT t.username
+              FROM DBA_USERS t
+              WHERE t.username LIKE 'INTEGRATION_TEST_%'
+                AND TRUNC(t.created) <= TRUNC(SYSDATE))
+        LOOP
+            EXECUTE IMMEDIATE 'DROP USER ' || i.username || ' CASCADE';
+        END LOOP;
+EXCEPTION
+    WHEN OTHERS THEN
+        dbms_output.put_line(SQLERRM);
+END;
+
+/*
+SELECT 'DROP USER ' || username || ' CASCADE;', t.*
+FROM All_Users t
+WHERE t.username LIKE 'INTEGRATION_TEST_%'
+  AND TRUNC(t.created) < TRUNC(SYSDATE)
+ORDER BY t.created DESC;
+*/
